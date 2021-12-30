a0ac11b58db9e6a829ec2d7a6f610fc53ad76ad0
akattipa <akattipa>
2021-12-30 15:12:16 +0530

    updated the comments in api_db projects main file.

--- api_db_project/main_api_db.py
+++ api_db_project/main_api_db.py
@@ -1,3 +1,10 @@
+"""
+This project fetches the data such as name, email and interests of a registered user from database,
+and then connects to an api and gathers the news on the selected interest from that api and aggregates
+all the data and sends the news with the Title and the URL of that news to the user as an email.
+
+"""
+
 from api_db_project.SQLData import Connection
 from api_db_project.FetchNews import News
 from api_db_project.EmailFunction import SendNewsEmails
